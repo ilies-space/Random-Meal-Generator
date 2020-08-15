3297a58e04aa55d905fe019f615f178c6d7cd930
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-08-15 15:43:42 +0100

    Ui scatch Optimisation

--- App.js
+++ App.js
@@ -1,35 +1,44 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, Button, ScrollView, Image} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 
 const App: () => React$Node = () => {
   // functions goas here
+
+  function getDataFromApi() {
+    console.log('getDataFromApi');
+  }
+
+  //const goas here
+  const [meal, setMeal] = useState();
 
   const [title, setTitle] = useState('Title Exmple !');
   const [img, setImg] = useState(
     'https://www.themealdb.com/images/media/meals/wrpwuu1511786491.jpg',
   );
   const [instructions, setInstructions] = useState(
-    'Instruction exmple Ipsum duis velit id laborum culpa magna magna aute culpa Culpa eiusmod proident incididunt ut dolore dolor esse culpa excepteur reprehenderit ad anim sint..',
+    'Instruction exmple Ipsum duis velit id Do sit Et ad Nulla veniam in dolor commodo culpa ex excepteur magna reprehenderit occaecat dolor excepteur laborum. Incididunt qui nisi exercitation ullamco officia consectetur labore. Consequat proident excepteur aliqua nulla id dolore laboris et mollit nostrud fugiat tempor qui. Ea laboris nisi occaecat aliqua eu. Do elit ut ad quis occaecat. aliqua veniam sit. elit sunt est ipsum. laborum culpa magna magna aute culpa Culpa eiusmod proident incididunt ut dolore dolor esse culpa excepteur reprehenderit ad anim sint..',
   );
 
   return (
     <View style={styles.container}>
-      <Button
-        title="get meal !"
-        onPress={() => console.log('Get randome MEal')}
-      />
       <View
         style={{
           flexDirection: 'row',
           height: 150,
-          backgroundColor: 'red',
           width: '100%',
         }}>
         <View
           style={{
             flexDirection: 'row',
             height: 150,
-            backgroundColor: 'yellow',
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
@@ -44,7 +53,6 @@ const App: () => React$Node = () => {
         <View
           style={{
             height: 150,
-            backgroundColor: 'gray',
             flex: 1,
             justifyContent: 'center',
           }}>
@@ -54,14 +62,13 @@ const App: () => React$Node = () => {
         </View>
       </View>
       <View style={{padding: 10, flex: 1, width: '100%'}}>
-        <ScrollView
-          style={{width: '100%', padding: 10, backgroundColor: 'red', flex: 1}}>
-          <View style={{padding: 10, backgroundColor: 'red'}}>
+        <ScrollView style={{width: '100%', padding: 10, flex: 1}}>
+          <View style={{padding: 10}}>
             <Text>{instructions}</Text>
           </View>
         </ScrollView>
 
-        <View style={{width: '100%'}}>
+        <View style={{marginLeft: 20, paddingVertical: 5}}>
           <Text style={{fontWeight: 'bold', fontSize: 18}}>Integredients</Text>
 
           <Text>- Exmple 1</Text>
@@ -72,13 +79,17 @@ const App: () => React$Node = () => {
 
       <View
         style={{
-          backgroundColor: 'red',
           flex: 1,
-          width: '100%',
+          width: '90%',
           alignItems: 'center',
           justifyContent: 'center',
+          backgroundColor: '#ecf0f1',
         }}>
         <Text>Youtube video goas here !</Text>
+      </View>
+
+      <View style={{margin: 10}}>
+        <Button title="get meal !" onPress={() => getDataFromApi()} />
       </View>
     </View>
   );
@@ -89,6 +100,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 5,
   },
 });
 
