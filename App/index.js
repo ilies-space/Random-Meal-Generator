4538bc7ed118d9497550793c0600f26a945a932c
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-08-21 12:17:48 +0100

    Folder structure improvments

---
+++ App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import {Main} from '../src/Componenets/Main';
+
+export default () => {
+  return (
+    <View style={{flex: 1}}>
+      <Main />
+    </View>
+  );
+};
